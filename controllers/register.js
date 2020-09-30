ce51ebf01782e630d764f03e9511993b11a90e6a
Spencer Broden <9060485+SpencerBroden@users.noreply.github.com>
2020-09-30 16:01:38 -0700

    register.js update

--- controllers/register.js
+++ controllers/register.js
@@ -1,8 +1,8 @@
 const handleRegister = (req, res, db, bcrypt) => {
+  const { name, email, password } = req.body;
   if (!email || !name || !password) {
       return res.status(400).json('incorrect form submission');
   }
-  const { name, email, password } = req.body;
   const hash = bcrypt.hashSync(password);
   db.transaction(trx => {
       trx.insert({
